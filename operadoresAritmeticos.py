f9dfbc4930983b0505db218eaf3f264ef76d14d0
FRANCISCO CARLOS NASCIMENTO DA SILVA <202408155069@alunos.unifanor.edu.br>
2025-03-28 10:15:50 -0300

    primeiro commit

---
+++ operadoresAritmeticos.py
@@ -0,0 +1,39 @@
+# Adição
+numero1 = 10
+numero2 = 20
+
+soma = numero1 + numero2
+print(soma)
+
+# subtração
+
+subtracao = numero1 - numero2
+print(subtracao)
+      
+# multiplicação
+
+multiplicacao = numero1 * numero2
+print(multiplicacao)
+
+# Divisão 
+
+divisao = numero1 / numero2 
+print(divisao)
+
+# Potencia
+
+potencia = numero1 ** numero2 
+print(potencia) 
+
+# Modulo
+
+modulo = numero1 % numero2 
+print(modulo)
+
+# Resto da divisao
+
+restodadivisao = numero1 // numero2 
+print(restodadivisao)
+
+
+      
