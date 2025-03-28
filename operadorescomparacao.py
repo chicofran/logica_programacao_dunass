9ab18dc79739b45aa83ae07ad1239f5fa23078a2
FRANCISCO CARLOS NASCIMENTO DA SILVA <202408155069@alunos.unifanor.edu.br>
2025-03-28 10:55:20 -0300

    operadores de comparacao

---
+++ operadorescomparacao.py
@@ -0,0 +1,20 @@
+x = 10
+y = 10
+
+# comparacao de igualdade
+print(x == y)
+
+# superioridade (>)
+print(x > y)
+
+# diferença (!=)
+print(x != y)
+
+# inferioridade (<)
+print(x < y)
+
+# maior ou igual (>=)
+print(x <= y)
+
+#menor ou igual (<=)
+print(x <= y)
